ec60b67012d551249fe1ea030001e139471306e1
WSU99 <49138207+WSU99@users.noreply.github.com>
2019-04-19 18:18:59 -0300

    Add files via upload

--- codigo de movimento simples/quadrado/Inimigo.py
+++ codigo de movimento simples/quadrado/Inimigo.py
@@ -10,3 +10,15 @@ Parado = pyganim.PygAnimation([('images\Inimigo\L1E.png', 100),
                                  ('images\Inimigo\L8E.png', 100),
                                  ('images\Inimigo\L10E.png', 100),
                                  ('images\Inimigo\L11E.png', 100)])
+
+Direita = pyganim.PygAnimation([('images\Inimigo\R1E.png', 100),
+                                 ('images\Inimigo\R2E.png', 100),
+                                 ('images\Inimigo\R3E.png', 100),
+                                 ('images\Inimigo\R4E.png', 100),
+                                 ('images\Inimigo\R5E.png', 100),
+                                 ('images\Inimigo\R6E.png', 100),
+                                 ('images\Inimigo\R7E.png', 100),
+                                 ('images\Inimigo\R8E.png', 100),
+                                 ('images\Inimigo\R9E.png', 100),
+                                 ('images\Inimigo\R10E.png', 100),
+                                 ('images\Inimigo\R11E.png', 100)])
